6292887a786d2a5633cc89523e8bb2ef563154ee
dariganarmanova <narmanovadariga@gmail.com>
2024-08-07 02:21:09 +0500

    did login and signup api

---
+++ backend/routes/auth.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { User } = require('./models/user');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs'); // For hashing passwords
+const router = express.Router();
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ mssg: 'User already exists' });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        user = new User({ email, password: hashedPassword });
+        await user.save();
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.status(201).json({ token })
+    } catch (error) {
+        res.status(500).send('Server error');
+    }
+})
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(400).json("Invalid credentials");
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (error) {
+        res.status(500).send('Server error');
+
+    }
+});
+module.exports = router;
