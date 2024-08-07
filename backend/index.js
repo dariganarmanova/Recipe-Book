6c639893419670bcf20da544137a0a04a7465efc
dariganarmanova <narmanovadariga@gmail.com>
2024-08-08 02:12:32 +0500

    upd apis

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const { connectToDatabase } = require('./db/mongoose');
 const cors = require('cors');
+const auth = require('./routes/auth');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-connectToDatabase();
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
 
+connectToDatabase();
 //app.use('/login', login);
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.use('/api', auth);
+
 
 app.listen(8002, () => {
     console.log("Server is running on port 8002");
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { User } = require('./models/user');
+const { User } = require('../models/user');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs'); // For hashing passwords
 const router = express.Router();
