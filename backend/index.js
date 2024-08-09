ab42d4d49d6b21c34815093db7cec12fd8c562e7
dariganarmanova <narmanovadariga@gmail.com>
2024-08-09 15:37:49 +0500

    fixed some bugs

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
-}));
+app.use(cors());
+
 
 connectToDatabase();
 //app.use('/login', login);
